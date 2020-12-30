26215dafcadcb91072d376d3c2303e2917200139
Neal Stanard <nstanard@gmail.com>
2020-12-30 15:00:44 -0600

    FE-1294 - use custom filter from draft

--- src/pages/domains/helpers/customDomainStatusFilter.js
+++ src/pages/domains/helpers/customDomainStatusFilter.js
@@ -8,23 +8,16 @@ const customDomainStatusFilter = function(rows, columnIds, value) {
   const tableColumnName = columnIds[0];
   const mappedRows = rows
     .map(row => {
-      let atLeastOneIsFilteredOut = 0;
-      Object.keys(value).forEach(status => {
-        if (status !== 'blocked' && row.values[tableColumnName]['blocked']) return; // blocked gets filtered out regardless of other applied filters
-        if (status !== 'unverified' && row.values[tableColumnName]['unverified']) return; // unverified gets filtered out regardless of other applied filters
-
-        // When verified (readyForSending) gets filtered out, we still need to consider the other filters applied
-        if (status === 'readyForSending' && !appliedFilters[status]) {
-          if (row.values[tableColumnName]['readyForDKIM']) return;
-          if (row.values[tableColumnName]['readyForBounce']) return;
-          if (row.values[tableColumnName]['validSPF']) return;
-          atLeastOneIsFilteredOut++;
-        } else if (!appliedFilters[status] && row.values[tableColumnName][status]) {
-          atLeastOneIsFilteredOut++;
+      let trueForStleastOne = 0;
+      Object.keys(appliedFilters).forEach(status => {
+        if (status !== 'blocked' && row.values[tableColumnName]['blocked']) return;
+        //if a domain is blocked we don't have to compare other fields
+        else if (appliedFilters[status] && row.values[tableColumnName][status]) {
+          trueForStleastOne++;
+          return;
         }
       });
-
-      return atLeastOneIsFilteredOut ? undefined : row;
+      return trueForStleastOne > 0 ? row : false;
     })
     .filter(Boolean);
 
