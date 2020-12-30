a7b1ace48a41cd51bfa85186b2b08c07854420ea
shrirupa <shrirupa.chowdhury@sparkpost.com>
2020-12-30 09:24:47 -0500

    [FE-1251] Removed useEmptyState from api keys

--- src/pages/api-keys/ListPage.container.js
+++ src/pages/api-keys/ListPage.container.js
@@ -13,50 +13,12 @@ import { selectKeysForAccount } from 'src/selectors/api-keys';
 // Components/helpers
 import { isAccountUiOptionSet } from 'src/helpers/conditions/account';
 import { ListPage } from './ListPage';
-import { useEmptyState } from 'src/hooks';
-import APIKeysEmptyState from './components/EmptyState';
-import { Loading } from 'src/components';
 import { useHibana } from 'src/context/HibanaContext';
 
 function ListPageContainer(props) {
-  const {
-    keys,
-    loading,
-    hasSubaccounts,
-    subaccounts,
-    isEmptyStateEnabled,
-    listApiKeys,
-    listSubaccounts,
-    hideNewApiKey,
-  } = props;
   const [{ isHibanaEnabled }] = useHibana();
 
-  React.useEffect(() => {
-    listApiKeys();
-
-    return () => {
-      // only want to show the new key after a create
-      hideNewApiKey();
-    };
-  }, [hideNewApiKey, listApiKeys]);
-
-  React.useEffect(() => {
-    if (hasSubaccounts && subaccounts.length === 0) {
-      listSubaccounts();
-    }
-  }, [hasSubaccounts, listSubaccounts, subaccounts]);
-
-  const component = useEmptyState({
-    isEmpty: keys.length === 0 && isEmptyStateEnabled && isHibanaEnabled,
-    pageComponent: ListPage,
-    emptyComponent: APIKeysEmptyState,
-  })({ ...props, isHibanaEnabled });
-
-  if (loading) {
-    return <Loading />;
-  }
-
-  return component;
+  return <ListPage isHibanaEnabled={isHibanaEnabled} {...props} />;
 }
 
 const mapDispatchToProps = { listApiKeys, hideNewApiKey, listSubaccounts };
