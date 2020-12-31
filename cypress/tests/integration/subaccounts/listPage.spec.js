a11aba1573066550107e8e2a7f1230f3be387268
Neal Stanard <nstanard@gmail.com>
2020-12-31 16:11:44 -0600

    FE-1252 - improved cypress tests

--- cypress/tests/integration/subaccounts/listPage.spec.js
+++ cypress/tests/integration/subaccounts/listPage.spec.js
@@ -1,4 +1,4 @@
-import { IS_HIBANA_ENABLED } from 'cypress/constants';
+import { IS_HIBANA_ENABLED, USERNAME } from 'cypress/constants';
 const SUBACCOUNT_API_URL = '/api/v1/subaccounts';
 const PAGE_URL = '/account/subaccounts';
 
@@ -19,6 +19,15 @@ function stubAccountsReq({ fixture = 'account/200.get.has-empty-states.json' } =
   });
 }
 
+// this is an override of the stub set by stubAuth
+function stubUsersRequest() {
+  cy.stubRequest({
+    url: `/api/v1/users/${USERNAME}`,
+    fixture: `users/200.get.subaccount-banner-dismissed.json`,
+    requestAlias: 'stubbedUsersRequest',
+  });
+}
+
 describe('The subaccounts list page', () => {
   beforeEach(() => {
     cy.stubAuth();
@@ -32,13 +41,15 @@ describe('The subaccounts list page', () => {
   });
 
   if (IS_HIBANA_ENABLED) {
-    it('does not render the empty state banner when "allow_empty_states" is not set', () => {
+    it('does not render the empty state when "allow_empty_states" is not set', () => {
       stubSubaccount({ fixture: '200.get.no-results.json' });
       cy.visit(PAGE_URL);
       cy.wait(['@subaccounts']);
       cy.title().should('include', 'Subaccounts');
       cy.findByRole('heading', { name: 'Manage your subaccounts' }).should('be.visible');
-      cy.findByText('Subaccounts are a good way of managing external client accounts.');
+      cy.findByText('Subaccounts are a good way of managing external client accounts.').should(
+        'be.visible',
+      );
       cy.verifyLink({
         content: 'Create Subaccount',
         href: '/account/subaccounts/create',
@@ -47,6 +58,20 @@ describe('The subaccounts list page', () => {
         content: 'Learn more',
         href: 'https://developers.sparkpost.com/api/subaccounts.html',
       });
+    });
+
+    it('does not renders the empty state banner when the banner has been dismissed', () => {
+      stubSubaccount({ fixture: 'subaccounts/200.get.json' });
+      stubAccountsReq();
+      stubUsersRequest(); // override for user ui option to turn off banner
+      cy.visit(PAGE_URL);
+      cy.wait(['@subaccounts']);
+      cy.title().should('include', 'Subaccounts');
+      cy.findByRole('heading', { name: 'Subaccounts' }).should('be.visible');
+      cy.findByRole('heading', { name: 'Organize Sending and Analytics' }).should('not.exist');
+      cy.findByText(
+        'Subaccounts can be used to provision and manage senders separately from each other, and to provide assets and reporting data for each of them.',
+      ).should('not.exist');
     });
 
     it('renders the empty state banner when "allow_empty_states" is set on the account and banner has not been dismissed', () => {
@@ -59,7 +84,7 @@ describe('The subaccounts list page', () => {
       cy.findByRole('heading', { name: 'Organize Sending and Analytics' }).should('be.visible');
       cy.findByText(
         'Subaccounts can be used to provision and manage senders separately from each other, and to provide assets and reporting data for each of them.',
-      );
+      ).should('be.visible');
       cy.verifyLink({
         content: 'Subaccounts Documentation',
         href: 'https://sparkpost.com/docs/user-guide/subaccounts/',
@@ -76,10 +101,16 @@ describe('The subaccounts list page', () => {
       cy.get('p').contains(
         'Subaccounts can be used to provision and manage senders separately from each other, and to provide assets and reporting data for each of them. Subaccounts are great for service providers who send on behalf of others or for businesses that want to separate different streams of traffic.',
       );
-      cy.findByText('Common uses for Subaccounts');
-      cy.findByText('Sending as a service provider for multiple unique customers.');
-      cy.findByText('Keeping unique internal business units independent from one another.');
-      cy.findByText('Tracking mission critical campaign data separate from other mailstreams.');
+      cy.findByText('Common uses for Subaccounts').should('be.visible');
+      cy.findByText('Sending as a service provider for multiple unique customers.').should(
+        'be.visible',
+      );
+      cy.findByText('Keeping unique internal business units independent from one another.').should(
+        'be.visible',
+      );
+      cy.findByText(
+        'Tracking mission critical campaign data separate from other mailstreams.',
+      ).should('be.visible');
       cy.verifyLink({
         content: 'Create Subaccount',
         href: '/account/subaccounts/create',
