d0aabf4138ed43f8664a8de1c9c3ae713addc4fd
Nick Lemmon <nick.lemmon@sparkpost.com>
2021-01-06 10:56:49 -0500

    TR-2938 - (UI) When publishing a template, immediately duplicating it before saving prevents duplication (#1971)
    * TR-2938 - Move templates tests to content subfolder
    
    * TR-2938 - Move domains tests to configuration subfolder
    
    * TR-2938 - Move helpers below tests
    
    * TR-2938 - Add failing test to reproduce bug
    
    * TR-2938 - Clean up tests to use cy.wait as well as more accurate element queries
    
    * TR-2938 - Resolve bug by using useParams instead of usePageFilters
    
    * TR-2938 - Fix failing test by mocking useParams
    
    * TR-2938 - Properly mock useParams in test
    
    * TR-2938 - Update obsolete tests, remove unnecessary snapshot
    
    * TR-2938 - Wrap up Cypress test with duplication
    
    * TR-2938 - Adjust UI semantics slightly, dismiss obscuring snackbar to help with test failure
    
    * TR-2938 - Using react-router-dom works after all
    
    * TR-2938 - Update import to react-router-dom instead of react-router

--- src/pages/templates/hooks/useEditorNavigation.js
+++ src/pages/templates/hooks/useEditorNavigation.js
@@ -1,23 +1,23 @@
 import { useMemo, useState } from 'react';
-import { useHistory } from 'react-router-dom';
+import { useHistory, useParams } from 'react-router-dom';
 import { usePageFilters } from 'src/hooks';
 import links from '../constants/editNavigationLinks';
 import { routeNamespace } from '../constants/routes';
 import { setSubaccountQuery } from '../../../helpers/subaccounts';
 
 const initFilters = {
-  id: {},
-  version: {},
   navKey: { excludeFromRoute: true },
   subaccount: {},
 };
 
 const useEditorNavigation = () => {
   const history = useHistory();
+  const { id, version = 'draft', navKey = '' } = useParams();
   const {
-    filters: { id, version = 'draft', navKey = '', subaccount: subaccountId },
+    filters: { subaccount: subaccountId },
   } = usePageFilters(initFilters);
   const [navKeyTemp, setNavKeyTemp] = useState(navKey.toLowerCase());
+
   const setNavigation = nextNavigationKey => {
     setNavKeyTemp(nextNavigationKey.toLowerCase());
     history.push(
