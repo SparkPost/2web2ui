eb192ad9cc51c9e513a2f3580cfce46fd60110e0
Nick Lemmon <nick.lemmon@sparkpost.com>
2021-01-16 14:24:54 -0500

    FE-1343 - Add remaining helpers to index

--- src/__testHelpers__/index.js
+++ src/__testHelpers__/index.js
@@ -1,2 +1,11 @@
+export { default as context } from './context';
+export { default as debugLog } from './debugLog';
 export { default as delay } from './delay';
+export { default as mockApi } from './mockApi';
+export { default as MockRecaptcha } from './mockRecaptcha';
 export { default as mockStore, createMockStore } from './mockStore';
+export { default as renderWithRedux } from './renderWithRedux';
+export { default as setupPortals } from './setupPortals';
+export { snapshotAction, snapshotActionCases } from './snapshotActionHelpers';
+export { default as TestApp } from './TestApp';
+export { default as Time } from './time';
