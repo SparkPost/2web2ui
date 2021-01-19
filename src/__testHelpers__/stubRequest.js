552fbc3cc3458ec9b519f38412887d8276d427ff
Nick Lemmon <nick.lemmon@sparkpost.com>
2021-01-19 14:10:39 -0500

    FE-1327 - Replace req base URL with fake-api-test-host

--- src/__testHelpers__/stubRequest.js
+++ src/__testHelpers__/stubRequest.js
@@ -11,7 +11,7 @@ import nock from 'nock';
  *
  */
 export default function stubRequest({
-  url = 'http://api.sparkpost.test',
+  url = 'http://fake-api-test-host.com',
   path,
   method = 'GET',
   statusCode = 200,
