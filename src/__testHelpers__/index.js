303212aca0160e48edb9981bd5e2a23cdbcfcbb1
Nick Lemmon <nick.lemmon@sparkpost.com>
2021-01-19 12:10:43 -0500

    FE-1327 - Properly export stubRequest

--- src/__testHelpers__/index.js
+++ src/__testHelpers__/index.js
@@ -5,5 +5,6 @@ export { default as mockStore, createMockStore } from './mockStore';
 export { default as renderWithRedux } from './renderWithRedux';
 export { default as setupPortals } from './setupPortals';
 export { snapshotAction, snapshotActionCases } from './snapshotActionHelpers';
+export { default as stubRequest } from './stubRequest';
 export { default as TestApp } from './TestApp';
 export { default as Time } from './time';
