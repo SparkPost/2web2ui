8a080e9fae2e691bc3a129eda3a598037d752cce
Patrick Sison <patrick.m.sison@gmail.com>
2021-01-20 11:11:49 -0500

    SA-1117: Addressed feedback

--- src/selectors/metrics.js
+++ src/selectors/metrics.js
@@ -1,11 +1,1 @@
-import { createSelector } from 'reselect';
-import { isUserUiOptionSet } from 'src/helpers/conditions/user';
-import { selectCondition } from 'src/selectors/accessConditionState';
-
-export const selectFeatureFlaggedMetrics = createSelector(
-  [
-    selectCondition(isUserUiOptionSet('use-metrics-rollup')),
-    // add more metrics feature flags here
-  ],
-  () => ({ useMetricsRollup: true }),
-);
+export const selectFeatureFlaggedMetrics = () => ({ useMetricsRollup: true });
