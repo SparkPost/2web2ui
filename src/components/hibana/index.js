6af58fc47ceb91db53f6ab5c298283b3fb285076
Nick Lemmon <nick.lemmon@sparkpost.com>
2021-01-25 10:44:21 -0500

    patch/FE-1329 - Removes HibanaBanner (#2038)
    * patch/FE-1329 - Remove HibanaBanner
    
    * patch/FE-1329 - Address test failure on events page

--- src/components/hibana/index.js
+++ src/components/hibana/index.js
@@ -1,5 +1,4 @@
 export { default as BottomPad } from './BottomPad';
-export { default as HibanaBanner } from './HibanaBanner';
 export { default as OGOnlyWrapper } from './OGOnlyWrapper';
 export { default as HibanaStyleHandler } from './HibanaStyleHandler';
 export { default as Header } from './Header';
