94ff36b1dc32a19f0a1c3dea750af70e74fd2e4e
Neal Stanard <nstanard@gmail.com>
2021-01-26 16:33:21 -0600

    FE-1337 - always use getRelativeDates result if it's not custom

--- src/hooks/usePinnedRport/usePinnedReport.js
+++ src/hooks/usePinnedRport/usePinnedReport.js
@@ -35,11 +35,10 @@ export default function usePinnedReport(onboarding) {
   const reportOptionsWithDates = reportOptions => {
     const { relativeRange, precision } = reportOptions;
     const { from, to } = getRelativeDates(relativeRange, { precision });
-    // Note: when relativeRange is set to custom, getRelativeDates won't produce from and to, so reportOptions goes last in the return struct
     return {
-      from,
-      to,
       ...reportOptions,
+      from: relativeRange === 'custom' ? reportOptions.from : from,
+      to: relativeRange === 'custom' ? reportOptions.to : to,
     };
   };
 
