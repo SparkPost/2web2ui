341f1483136982a15ab4e66b2577b6446161ee23
Nick Lemmon <nick.lemmon@sparkpost.com>
2021-01-29 14:46:24 -0500

    FE-1299-rebuild - Remove Hibana-related logic from the Pagination component

--- src/components/collection/Pagination.js
+++ src/components/collection/Pagination.js
@@ -1,10 +1,8 @@
 import React from 'react';
 import { Box, Pagination } from 'src/components/matchbox';
-import styles from './Pagination.module.scss';
 import { DEFAULT_PER_PAGE_BUTTONS, DEFAULT_PAGE_RANGE } from 'src/constants';
 import PerPageButtons from './PerPageButtons';
 import SaveCSVButton from './SaveCSVButton';
-import { useHibana } from 'src/context/HibanaContext';
 
 const CollectionPagination = ({
   currentPage,
@@ -16,9 +14,6 @@ const CollectionPagination = ({
   perPageButtons,
   saveCsv,
 }) => {
-  const [state] = useHibana();
-  const { isHibanaEnabled } = state;
-
   const renderPageButtons = () => {
     if (data.length <= perPage) {
       return null;
@@ -36,26 +31,6 @@ const CollectionPagination = ({
 
   if (!currentPage) {
     return null;
-  }
-
-  if (!isHibanaEnabled) {
-    return (
-      <div>
-        <div className={styles.PageButtons} data-id="pagination-pages">
-          {renderPageButtons()}
-        </div>
-        <div className={styles.PerPageButtons} data-id="pagination-per-page">
-          <PerPageButtons
-            totalCount={data.length}
-            data={data}
-            perPage={perPage}
-            perPageButtons={perPageButtons}
-            onPerPageChange={onPerPageChange}
-          />
-          <SaveCSVButton data={data} saveCsv={saveCsv} />
-        </div>
-      </div>
-    );
   }
 
   return (
