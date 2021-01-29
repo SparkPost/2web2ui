0445e4125681f2d2f426ae11d0618633c56f9ded
Nick Lemmon <nick.lemmon@sparkpost.com>
2021-01-29 13:58:47 -0500

    FE-1299-rebuild-2 - Update Pagination layout

--- src/components/collection/Pagination.js
+++ src/components/collection/Pagination.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import { Box, Pagination } from 'src/components/matchbox';
-import styles from './Pagination.module.scss';
+import { Box, Columns, Column, Pagination } from 'src/components/matchbox';
 import { DEFAULT_PER_PAGE_BUTTONS, DEFAULT_PAGE_RANGE } from 'src/constants';
+import styles from './Pagination.module.scss';
 import PerPageButtons from './PerPageButtons';
 import SaveCSVButton from './SaveCSVButton';
 import { useHibana } from 'src/context/HibanaContext';
@@ -18,9 +18,10 @@ const CollectionPagination = ({
 }) => {
   const [state] = useHibana();
   const { isHibanaEnabled } = state;
+  const hasPagination = data.length >= perPage;
 
   const renderPageButtons = () => {
-    if (data.length <= perPage) {
+    if (!hasPagination) {
       return null;
     }
 
@@ -59,9 +60,17 @@ const CollectionPagination = ({
   }
 
   return (
-    <Box display="flex" justifyContent="space-between">
-      <Box data-id="pagination-pages">{renderPageButtons()}</Box>
-      <Box display="flex" alignItems="center" data-id="pagination-per-page">
+    <Columns collapseBelow="xs" align="center">
+      {/* Conditionally rendering the entire column is necessary to prevent spacing problems */}
+      {hasPagination ? <Column data-id="pagination-pages">{renderPageButtons()}</Column> : null}
+
+      <Box
+        as={Column}
+        data-id="pagination-per-page"
+        display="flex"
+        alignItems="center"
+        justifyContent={['flex-start', 'flex-start', 'flex-end']}
+      >
         <PerPageButtons
           totalCount={data.length}
           data={data}
@@ -71,7 +80,7 @@ const CollectionPagination = ({
         />
         <SaveCSVButton size="small" outline data={data} saveCsv={saveCsv} />
       </Box>
-    </Box>
+    </Columns>
   );
 };
 
