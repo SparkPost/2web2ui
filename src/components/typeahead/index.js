88a65653132784f79091adebe8d9b8b00764d8d6
Nick Lemmon <nick.lemmon@sparkpost.com>
2021-03-11 10:30:48 -0500

    FE-1369 - Adds index file for Typeaheads

---
+++ src/components/typeahead/index.js
@@ -0,0 +1,9 @@
+export { ComboBoxTypeahead as AsyncComboBoxTypeahead } from './AsyncComboBoxTypeahead';
+export { ComboBoxTypeahead } from './ComboBoxTypeahead';
+export { default as InboxPlacementTypeahead } from './InboxPlacementTypeahead';
+export { default as SendingDomainTypeahead } from './SendingDomainTypeahead';
+export { default as SubaccountTypeahead } from './SubaccountTypeahead';
+export { default as TemplateTypeahead } from './TemplateTypeahead';
+export { TimezoneTypeahead } from './TimezoneTypeahead';
+export { Typeahead } from './Typeahead';
+export { default as TypeSelect } from './TypeSelect';
