da784ddc80324b352ff9adfa51b7a85e8bf5aac3
shrirupa <shrirupa.chowdhury@sparkpost.com>
2021-03-12 09:31:01 -0500

    [AC-1865] remove the test from Modal.test.js

--- src/components/matchbox/tests/Modal.test.js
+++ src/components/matchbox/tests/Modal.test.js
@@ -2,11 +2,6 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import { useHibana } from 'src/context/HibanaContext';
 import Modal from '../Modal';
-import Panel from '../Panel';
-import { useModal } from 'src/hooks';
-import { render, fireEvent } from '@testing-library/react';
-import * as segmentHelpers from 'src/helpers/segment';
-jest.mock('src/helpers/segment');
 jest.mock('src/context/HibanaContext');
 
 function mockHibanaIsEnabled() {
@@ -66,52 +61,6 @@ describe('Modal and Modal.LEGACY', () => {
       const wrapper = subject();
 
       expect(wrapper).not.toHaveDisplayName('OGModal');
-    });
-  });
-
-  describe('segment track is called with correct arguements', () => {
-    const MockComponent = ({ legacy = true }) => {
-      const { closeModal, openModal, isModalOpen = false } = useModal();
-
-      if (!legacy) {
-        return (
-          <>
-            <button onClick={openModal}>Open Modal</button>
-
-            <Modal open={isModalOpen} onClose={closeModal}>
-              <Modal.Header showCloseButton={false}>TEST NEW MODAL</Modal.Header>
-              <Modal.Content>New Content</Modal.Content>
-            </Modal>
-          </>
-        );
-      }
-      return (
-        <>
-          <button onClick={openModal}>Open Modal</button>
-
-          <Modal.LEGACY open={isModalOpen} onClose={closeModal} title="TEST MODAL">
-            <Panel.LEGACY title="TEST MODAL"> content</Panel.LEGACY>
-          </Modal.LEGACY>
-        </>
-      );
-    };
-    it('for Legacy Modal', () => {
-      const { queryByText } = render(<MockComponent />);
-
-      fireEvent.click(queryByText('Open Modal'));
-
-      expect(segmentHelpers.segmentTrack).toHaveBeenCalledWith('Modal Opened', {
-        title: 'TEST MODAL',
-      });
-    });
-    it('for Modal', () => {
-      const { queryByText } = render(<MockComponent legacy={false} />);
-
-      fireEvent.click(queryByText('Open Modal'));
-
-      expect(segmentHelpers.segmentTrack).toHaveBeenCalledWith('Modal Opened', {
-        title: 'TEST NEW MODAL',
-      });
     });
   });
 });
