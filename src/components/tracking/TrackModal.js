24b04eb424ec819c3aa2c2d15a05a30a35619fa5
shrirupa <shrirupa.chowdhury@sparkpost.com>
2021-03-15 17:05:45 -0400

    [AC-1865] Added location along with name in TrackModal

--- src/components/tracking/TrackModal.js
+++ src/components/tracking/TrackModal.js
@@ -1,18 +1,20 @@
 import React, { useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
 import { segmentTrack, SEGMENT_EVENTS } from 'src/helpers/segment';
 import { usePrevious } from 'src/hooks';
 
 export const TrackModal = ({ open, title, children }) => {
   const wasOpen = usePrevious(open);
+  const location = useLocation();
   useEffect(() => {
     if (!wasOpen && open) {
-      segmentTrack(SEGMENT_EVENTS.MODAL_OPENED, { title: title });
+      segmentTrack(SEGMENT_EVENTS.MODAL_OPENED, { title: title, location: location.pathname });
     }
 
     if (wasOpen && !open) {
-      segmentTrack(SEGMENT_EVENTS.MODAL_CLOSED, { title: title });
+      segmentTrack(SEGMENT_EVENTS.MODAL_CLOSED, { title: title, location: location.pathname });
     }
-  }, [open, title, wasOpen]);
+  }, [location.pathname, open, title, wasOpen]);
 
   return <>{children}</>;
 };
