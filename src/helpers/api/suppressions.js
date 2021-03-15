92bb329b1d83ae5185839950d002576be26d4507
Nick Lemmon <nick.lemmon@sparkpost.com>
2021-03-15 13:44:01 -0400

    FE-1369 - 'recipients' parameter defaults to an empty array

--- src/helpers/api/suppressions.js
+++ src/helpers/api/suppressions.js
@@ -4,7 +4,7 @@ import { setSubaccountHeader } from 'src/helpers/subaccounts';
 const LIKE_NON = new RegExp('non', 'i');
 const LIKE_TRUE = new RegExp('true', 'i');
 
-export function createOrUpdateSuppressions(recipients, subaccount) {
+export function createOrUpdateSuppressions(recipients = [], subaccount) {
   const sanitizedRecipients = recipients.map(
     ({ description, email, non_transactional, recipient, transactional, type }) => {
       // Convert deprecated type fields
