5aa97d99c33ef954b6b6505d36ee617eeb4a9f99
Nick Lemmon <nick.lemmon@sparkpost.com>
2021-03-15 16:24:22 -0400

    FE-1369 - Adds supporting virtual DOM tests for useSparkPostMutation

--- src/hooks/api/tests/useSparkPostMutation.test.js
+++ src/hooks/api/tests/useSparkPostMutation.test.js
@@ -44,13 +44,97 @@ describe('useSparkPostMutation', () => {
     expect(result.current.data).toEqual(FIXTURE.results);
   });
 
-  it('resets returned data to the initial state using the returned "reset" function', () => {});
+  it('returns error messages when mutations fail', async () => {
+    stubRequest({
+      method: 'POST',
+      path: '/v1/some/fake/endpoint',
+      fixture: {},
+      statusCode: 400,
+    });
 
-  it('returns error messages when mutations fail', () => {});
+    const { result, waitFor } = renderHook(
+      () => useSparkPostMutation(mockPostFn, DEFAULT_OPTIONS),
+      { wrapper },
+    );
+    result.current.mutate({ some: 'other data' });
 
-  it('calls the passed in "onMutate" handler when the mutation succeeds', () => {});
+    await waitFor(() => result.current.status === 'error');
 
-  it('calls the passed in "onSuccess" handler when the mutation succeeds', () => {});
+    expect(result.current.error.message).toBe('Request failed with status code 400');
+  });
 
-  it('calls the passed in "onError" handler when the mutation fails', () => {});
+  it('calls the passed in "onMutate" handler when the mutation succeeds', async () => {
+    const mockOnMutate = jest.fn();
+
+    stubRequest({
+      method: 'POST',
+      path: '/v1/some/fake/endpoint',
+      fixture: FIXTURE,
+    });
+
+    const { result, waitFor } = renderHook(
+      () => useSparkPostMutation(mockPostFn, { ...DEFAULT_OPTIONS, onMutate: mockOnMutate }),
+      { wrapper },
+    );
+    result.current.mutate({ some: 'data' });
+
+    await waitFor(() => result.current.status === 'success');
+
+    expect(mockOnMutate).toHaveBeenCalled();
+  });
+
+  it('calls the passed in "onSuccess" handler when the mutation succeeds', async () => {
+    const mockOnError = jest.fn();
+    const mockOnSuccess = jest.fn();
+
+    stubRequest({
+      method: 'POST',
+      path: '/v1/some/fake/endpoint',
+      fixture: FIXTURE,
+    });
+
+    const { result, waitFor } = renderHook(
+      () =>
+        useSparkPostMutation(mockPostFn, {
+          ...DEFAULT_OPTIONS,
+          onError: mockOnError,
+          onSuccess: mockOnSuccess,
+        }),
+      { wrapper },
+    );
+    result.current.mutate({ some: 'data' });
+
+    await waitFor(() => result.current.status === 'success');
+
+    expect(mockOnError).not.toHaveBeenCalled();
+    expect(mockOnSuccess).toHaveBeenCalled();
+  });
+
+  it('calls the passed in "onError" handler when the mutation fails', async () => {
+    const mockOnSuccess = jest.fn();
+    const mockOnError = jest.fn();
+
+    stubRequest({
+      method: 'POST',
+      path: '/v1/some/fake/endpoint',
+      fixture: {},
+      statusCode: 400,
+    });
+
+    const { result, waitFor } = renderHook(
+      () =>
+        useSparkPostMutation(mockPostFn, {
+          ...DEFAULT_OPTIONS,
+          onSuccess: mockOnSuccess,
+          onError: mockOnError,
+        }),
+      { wrapper },
+    );
+    result.current.mutate({ some: 'data' });
+
+    await waitFor(() => result.current.status === 'error');
+
+    expect(mockOnSuccess).not.toHaveBeenCalled();
+    expect(mockOnError).toHaveBeenCalled();
+  });
 });
