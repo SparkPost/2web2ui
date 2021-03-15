2d45e4b0bc27cf53a6b0aa0bd21a37c25d39b6b9
shrirupa <shrirupa.chowdhury@sparkpost.com>
2021-03-15 12:48:26 -0400

    [AC-1865] Remove unused ActionsModal

--- src/components/modals/index.js
+++ src/components/modals/index.js
@@ -1,4 +1,3 @@
-export { default as ActionsModal } from './ActionsModal';
 export { default as AsyncActionModal } from './AsyncActionModal';
 export { default as ConfirmationModal } from './ConfirmationModal';
 export { default as DeleteModal } from './DeleteModal';
