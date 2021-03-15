ddf93221987bd09cd830ccd34111bcc42796ca2a
Nick Lemmon <nick.lemmon@sparkpost.com>
2021-03-15 15:30:47 -0400

    FE-1369 - Update failing test

--- src/components/globalAlert/tests/Alert.test.js
+++ src/components/globalAlert/tests/Alert.test.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import { ButtonLink } from 'src/components/links';
 import Alert from '../Alert';
 import { shallow } from 'enzyme';
 
 describe('Alert', () => {
-  window.setTimeout = jest.fn((a) => a);
-  window.clearTimeout = jest.fn((a) => a);
+  window.setTimeout = jest.fn(a => a);
+  window.clearTimeout = jest.fn(a => a);
 
   const props = {
     autoDismiss: true,
@@ -12,7 +13,7 @@ describe('Alert', () => {
     message: 'message',
     type: 'error',
     details: 'details',
-    onDismiss: jest.fn()
+    onDismiss: jest.fn(),
   };
 
   let wrapper;
@@ -21,42 +22,26 @@ describe('Alert', () => {
     wrapper = shallow(<Alert {...props} />);
   });
 
-  it('should render with all props', () => {
-    expect(wrapper).toMatchSnapshot();
-    expect(wrapper.find('a')).toHaveLength(1);
-  });
-
-  it('should render without an action or details', () => {
-    wrapper.setProps({ details: null });
-    expect(wrapper).toMatchSnapshot();
-  });
-
-  it('should render a provided action', () => {
-    wrapper.setProps({
-      details: null,
-      action: { content: 'action content', onClick: jest.fn() }
-    });
-    expect(wrapper).toMatchSnapshot();
-  });
-
   it('should handle action on click', () => {
     wrapper.setProps({
       details: null,
-      action: { content: 'action content', onClick: jest.fn() }
+      action: { content: 'action content', onClick: jest.fn() },
     });
-    wrapper.find('a').simulate('click');
+    wrapper.find(ButtonLink).simulate('click');
     expect(wrapper.instance().props.action.onClick).toHaveBeenCalled();
   });
 
   it('should handle details click', () => {
-    wrapper.find('a').simulate('click');
-    expect(wrapper).toMatchSnapshot();
+    wrapper.find(ButtonLink).simulate('click');
     expect(wrapper).toHaveState('showDetails', true);
     expect(wrapper.instance().timeout).not.toBe(null);
 
     // refresh
     expect(window.clearTimeout).toHaveBeenCalledWith(wrapper.instance().timeout);
-    expect(window.setTimeout).toHaveBeenCalledWith(wrapper.instance().handleDismiss, props.timeoutInterval);
+    expect(window.setTimeout).toHaveBeenCalledWith(
+      wrapper.instance().handleDismiss,
+      props.timeoutInterval,
+    );
   });
 
   it('should handle dismiss', () => {
